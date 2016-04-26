995393497650ba5b5d6b88af236e8dfb32a51630
Ethan Han <zestime@gmail.com>
2016-04-26 10:14:27 +0900

    added a test for gridtreeConfig

--- build/main.spec.js
+++ build/main.spec.js
@@ -2,18 +2,21 @@
 
 describe('main', function () {
 
-    var scope, $compile, element;
-    var gridtreeConfig;
+  var scope, $compile, element;
+  var gridtreeConfig;
 
-    beforeEach(module('ui.gridtree'));
+  beforeEach(module('ui.gridtree'));
 
-    beforeEach(inject(function ($rootScope, _$compile_, _gridtreeConfig_) {
-        scope = $rootScope;
-        $compile = _$compile_;
-        gridtreeConfig = _gridtreeConfig_;
-    }));
+  beforeEach(inject(function ($rootScope, _$compile_, _gridtreeConfig_) {
+    scope = $rootScope;
+    $compile = _$compile_;
+    gridtreeConfig = _gridtreeConfig_;
+  }));
 
-    it('should defined gridtreeConfig.', function () {
-        return expect(gridtreeConfig).toBeDefined();
-    });
+  it('should defined gridtreeConfig.', function () {
+    return expect(gridtreeConfig).toBeDefined();
+  });
+  it('should return a default class name of columnClass in gridtreeConfig.', function () {
+    return expect(gridtreeConfig.columnClass).toEqual('gridtree-column');
+  });
 });
--- src/main.spec.js
+++ src/main.spec.js
@@ -1,17 +1,20 @@
 'use strict';
 
 describe('main', () => {
-    
-    var scope, $compile, element;
-    var gridtreeConfig;
-    
-    beforeEach(module('ui.gridtree'));
-    
-    beforeEach(inject(($rootScope, _$compile_, _gridtreeConfig_) => {
-        scope = $rootScope;
-        $compile = _$compile_;
-        gridtreeConfig = _gridtreeConfig_;
-    }));
-   
-   it('should defined gridtreeConfig.', () => expect(gridtreeConfig).toBeDefined()); 
+
+  var scope, $compile, element;
+  var gridtreeConfig;
+
+  beforeEach(module('ui.gridtree'));
+
+  beforeEach(inject(($rootScope, _$compile_, _gridtreeConfig_) => {
+    scope = $rootScope;
+    $compile = _$compile_;
+    gridtreeConfig = _gridtreeConfig_;
+  }));
+
+  it('should defined gridtreeConfig.', () => expect(gridtreeConfig).toBeDefined()); 
+  it('should return a default class name of columnClass in gridtreeConfig.', 
+    () => expect(gridtreeConfig.columnClass).toEqual('gridtree-column')); 
+
 });
